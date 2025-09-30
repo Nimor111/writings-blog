f7137e13d29ec9800ddc25e8342e826ed3d8712f
Nimor111 <Nimor111@users.noreply.github.com>
2025-09-30 21:57:29 +0000

    deploy: 4c9858d95c629504a66af45e3d16ad3d1342a086

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1759269449',resources: [
+      '/writings-blog/assets/css/jekyll-theme-chirpy.css',
+      '/writings-blog/',
+      
+        '/writings-blog/categories/',
+      
+        '/writings-blog/tags/',
+      
+        '/writings-blog/archives/',
+      
+        '/writings-blog/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
