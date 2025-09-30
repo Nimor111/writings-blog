fd8cefd275e8d288f71d53f514980ad16528766b
Nimor111 <georgi.bojinov@hotmail.com>
2025-09-30 22:04:19 +0000

    Deploying to gh-pages from @ Nimor111/writings-blog@986c1660fb9ab5930d09a8165e4b340801883d5d 🚀

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1759269684',resources: [
+    cacheName: 'chirpy-1759269858',resources: [
       '/writings-blog/assets/css/jekyll-theme-chirpy.css',
       '/writings-blog/',
       
